c5d405d8aed60e741481ca3cc7de504d950ec8b7
Sushant Pandey <110762985+DRACULA1729@users.noreply.github.com>
2025-01-06 14:44:52 +0530

    Create messagecreator.py
    Firstly run this file.

---
+++ messagecreator.py
@@ -0,0 +1,6 @@
+#This script writes a single input given number of time in message.txt. 
+with open ('message.txt', 'w') as f:
+    I=input("Enter your message: ")
+    n= int(input("Enter the number of times you want to send: ")
+    for i in range (n):
+        f.write(I +"\n")
